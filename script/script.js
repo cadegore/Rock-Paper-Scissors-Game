e9ab80c0b22d74d40611f87b82aaaee22bd79b9f
Cade Gore <cade_gore@icloud.com>
2022-06-27 23:39:56 -0400

    Rock, Paper, Scissors Game (v3.0)
    - First Version UI of Rock, Paper, Scissors Game
    - Game is able to play for one round
    - Able to reset to play more rounds

--- script/script.js
+++ script/script.js
@@ -2,99 +2,117 @@ const startGame = document.getElementById('start-game-btn');
 const rock = document.getElementById('rock');
 const scissors = document.getElementById('scissors');
 const paper = document.getElementById('paper');
-const choiceMessage = document.getElementById('choiceMessage');
+// const choiceMessage = document.getElementById('choiceMessage');
 const winnerMessage = document.getElementById('winnerMessage');
-const playAgain = document.getElementById('playAgain');
+const playAgain = document.querySelector('.playAgain');
 const gameInstructions = document.querySelector('.game-direction');
 const gameRules = document.querySelector('.game-rules');
-
-const choice = ['rock', 'scissors', 'paper'];
+const gameScreen = document.querySelector('.game-screen');
+const playerChoiceOptions = document.querySelector('.player-choice');
+const playerChoice = document.querySelector('.player-choice-screen');
+const computerChoice = document.querySelector('.computer-choice-screen');
+const choice = ['👊🏻', '✌🏻', '🖐🏻'];
 
 startGame.addEventListener('click', () => {
   console.log('Game started');
   startGame.classList.add('hidden');
   gameInstructions.classList.add('hidden');
+  gameScreen.classList.remove('hidden');
+  // gameScreen.style.display = 'flex';
   gameRules.classList.remove('hidden');
   rock.classList.remove('hidden');
   paper.classList.remove('hidden');
   scissors.classList.remove('hidden');
+  playerChoiceOptions.classList.remove('hidden');
 });
 
 rock.addEventListener('click', () => {
-  let computerChoice = choice[Math.floor(Math.random() * choice.length)];
-  choiceMessage.textContent = `rock vs ${computerChoice}`;
-  if (computerChoice === 'scissors') {
+  playerChoice.textContent = '👊🏻';
+  let computerChoose = choice[Math.floor(Math.random() * choice.length)];
+  computerChoice.textContent = computerChoose;
+  playerChoiceOptions.classList.add('hidden');
+  playAgain.classList.remove('hidden');
+  // choiceMessage.textContent = `rock vs ${computerChoose}`;
+  if (computerChoose === '✌🏻') {
     winnerMessage.textContent = 'Player win';
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
-  } else if (computerChoice === 'paper') {
+  } else if (computerChoose === '🖐🏻') {
     winnerMessage.textContent = 'Computer win';
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
   } else {
     winnerMessage.textContent = "It's a tie";
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
   }
 });
 scissors.addEventListener('click', () => {
-  let computerChoice = choice[Math.floor(Math.random() * choice.length)];
-  choiceMessage.textContent = `scissors vs ${computerChoice}`;
-  if (computerChoice === 'paper') {
+  playerChoice.textContent = '✌🏻';
+  let computerChoose = choice[Math.floor(Math.random() * choice.length)];
+  computerChoice.textContent = computerChoose;
+  playerChoiceOptions.classList.add('hidden');
+  playAgain.classList.remove('hidden');
+  // choiceMessage.textContent = `scissors vs ${computerChoose}`;
+  if (computerChoose === '🖐🏻') {
     winnerMessage.textContent = 'Player win';
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
-  } else if (computerChoice === 'rock') {
+  } else if (computerChoose === '👊🏻') {
     winnerMessage.textContent = 'Computer win';
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
   } else {
     winnerMessage.textContent = "It's a tie";
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
   }
 });
 paper.addEventListener('click', () => {
-  let computerChoice = choice[Math.floor(Math.random() * choice.length)];
-  choiceMessage.textContent = `paper vs ${computerChoice}`;
-  if (computerChoice === 'rock') {
+  playerChoice.textContent = '🖐🏻';
+  let computerChoose = choice[Math.floor(Math.random() * choice.length)];
+  computerChoice.textContent = computerChoose;
+  playerChoiceOptions.classList.add('hidden');
+  playAgain.classList.remove('hidden');
+  // choiceMessage.textContent = `paper vs ${computerChoose}`;
+  if (computerChoose === '👊🏻') {
     winnerMessage.textContent = 'Player win';
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
-  } else if (computerChoice === 'scissors') {
+  } else if (computerChoose === '✌🏻') {
     winnerMessage.textContent = 'Computer win';
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
   } else {
     winnerMessage.textContent = "It's a tie";
-    rock.setAttribute('disabled', true);
-    scissors.setAttribute('disabled', true);
-    paper.setAttribute('disabled', true);
+    // rock.setAttribute('disabled', true);
+    // scissors.setAttribute('disabled', true);
+    // paper.setAttribute('disabled', true);
     playAgain.removeAttribute('hidden');
   }
 });
 
 playAgain.addEventListener('click', () => {
-  rock.removeAttribute('disabled');
-  scissors.removeAttribute('disabled');
-  paper.removeAttribute('disabled');
-  playAgain.setAttribute('hidden', true);
-  choiceMessage.textContent = '';
+  startGame.classList.remove('hidden');
+  gameScreen.classList.add('hidden');
   winnerMessage.textContent = '';
+  playAgain.classList.add('hidden');
+  playerChoice.textContent = '';
+  computerChoice.textContent = '';
 });
